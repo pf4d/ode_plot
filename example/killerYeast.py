56a12b80746016fc1c759388e5fffdcbd8b6ccb2
Evan Cummings <evan.cummings@umontana.edu>
2013-12-08 15:22:48 -0700

    finish killer_yeast project

--- example/killerYeast.py
+++ example/killerYeast.py
@@ -1,17 +1,24 @@
 import sys
 sys.path.append('../')
 
-from scipy.integrate._ode import ode
-from src.ode import dirField, dirField_2, Euler
-from pylab import *
-from mpl_toolkits.mplot3d import Axes3D
+from scipy.integrate._ode    import ode
+from src.ode                 import dirField, dirField_2, Euler
+from pylab                   import *
+from mpl_toolkits.mplot3d    import Axes3D
+from multiprocessing         import Queue, cpu_count, Process
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 #---------------------------------------------------------------------
 # ODE function to be integrated
 def dLdt(L, S, C, params):
   """
   INPUT:
+    L - population of killer yeast.
+    S - population of sensitive yeast.
+    C - population of nutrient.
+    params - dLdt equation parameters.
   OUTPUT:
+    dLdt - time derivative of L.
   """
   K_L   = params[0]
   F     = params[1]
@@ -22,7 +29,12 @@ def dLdt(L, S, C, params):
 def dSdt(L, S, C, params):
   """
   INPUT:
+    L - population of killer yeast.
+    S - population of sensitive yeast.
+    C - population of nutrient.
+    params - dSdt equation parameters.
   OUTPUT:
+    dSdt - time derivative of S.
   """
   K_S   = params[0]
   F     = params[1]
@@ -34,7 +46,12 @@ def dSdt(L, S, C, params):
 def dCdt(L, S, C, params):
   """
   INPUT:
+    L - population of killer yeast.
+    S - population of sensitive yeast.
+    C - population of nutrient.
+    params - dCdt equation parameters.
   OUTPUT:
+    dCdt - time derivative of C.
   """
   a_L  = params[0]
   a_S  = params[1]
@@ -69,14 +86,11 @@ def f(t, y, dLdt, dSdt, dCdt, L_params, S_params, C_params):
   rhs3 = dCdt(L, S, C, C_params)    # right hand side 3rd eqn
   return array([rhs1, rhs2, rhs3])
  
-
-# Initial conditions
-y0 = [0.3, 0.3, 0.001]  
-
-def model(F, beta, tf, dt):
+def model(F, beta, y0, ta, dt):
   """
   Run model for given volume flow rate <F> and toxin coef <beta> for total
-  time <tf> in hours at timestep <dt>, also in hours.
+  time array <ta> in hours at timestep <dt>, also in hours.  Returns the 
+  last solution 3-tuple for L, S, and C.
   """ 
   # Additional parameters being passed to the ODE function
   a_L  = 0.1124
@@ -89,15 +103,11 @@ def model(F, beta, tf, dt):
   L_params = [K_L, F, V]
   S_params = [K_S, F, V, beta]
   C_params = [a_L, a_S, K_L, K_S, F, V, C_0]
- 
-  t0  = 0.0                          # initial time 
-  ta  = arange(t0, tf+dt, dt)        # Time span
-  ys  = linspace(0, max(y0), 1000)   # ys for plotting dy/dt
   
   # Call function that integrates the ODE:
   r = ode(f)
   r.set_integrator('dopri5', atol=1e-6, rtol=1e-5)
-  r.set_initial_value(y0, t0)
+  r.set_initial_value(y0, ta)
   r.set_f_params(dLdt, dSdt, dCdt, L_params, S_params, C_params)
   
   sol = []
@@ -107,23 +117,153 @@ def model(F, beta, tf, dt):
     sol.append(r.y)
   sol = array(sol).T
   
-  return ta, sol
+  return sol[:,-1]
 
-ta, sol = model(F=0.02, beta=0.009134, tf=40000, dt=2)
 
-#Plot the solution:
-fig = figure()
-ax1 = fig.add_subplot(111)
-ax1.plot(ta, sol[0], 'r-',  lw=2.0, label=r'$L$')
-ax1.plot(ta, sol[1], 'k-',  lw=2.0, label=r'$S$')
-ax1.plot(ta, sol[2], 'k--', lw=2.0, label=r'$C$')
+class solveProcess(Process):
+  """
+  Process to solve the model function.
+  """
+  def __init__(self, i, queue, beta_a, F_a, y0, ta, dt, p):
+    """
+    Initialize the Process with ID <i>, processing queue <queue>, beta array
+    <beta_a>, flow array <F_a>, time array <ta>, timestep <dt>, and number of
+    parameters <p>.
+    """
+    Process.__init__(self)
+    self.i      = i
+    self.q      = queue
+    self.beta_a = beta_a
+    self.F_a    = F_a
+    self.y0     = y0
+    self.ta     = ta
+    self.dt     = dt
+    self.m      = len(beta_a)
+    self.n      = len(F_a)
+    self.p      = p
+  
+  def run(self):
+    """
+    solve the differential equations for all beta_a and F_a.
+    """
+    p = self.p
+    m = self.m
+    n = self.n
 
-ax1.set_xlabel(r'$t$')
-ax1.set_ylabel(r'Optical Density')
-ax1.set_title('Solution')
-leg = ax1.legend(loc='upper right')
-leg.get_frame().set_alpha(0.5)
-ax1.grid()
+    SS_L = zeros((m,n))
+    SS_S = zeros((m,n))
+    SS_C = zeros((m,n))
+
+    for i, beta in enumerate(self.beta_a):
+      for j, F in enumerate(self.F_a):
+        print 'Process %i solving: beta=%f, F=%f' % (self.i, beta, F)
+        sol = model(F=F, beta=beta, y0=self.y0, ta=self.ta, dt=self.dt)
+        SS_L[i,j] = sol[0]
+        SS_S[i,j] = sol[1]
+        SS_C[i,j] = sol[2]
+
+    self.q.put(array([SS_L, SS_S, SS_C]))  # add the result to the queue.
+
+def plot_sol(ax, f, extent, tit, cmap='Greys'):
+  """
+  plot the 2D solution <f> to axes <ax>.
+  """
+  im      = ax.imshow(f, extent=extent, cmap=cmap)
+  divider = make_axes_locatable(ax)
+  cax     = divider.append_axes("right", size="5%", pad=0.05)
+  ax.set_title(tit)
+  ax.set_xlabel(r'$\beta$')
+  ax.set_ylabel(r'$F$')
+  colorbar(im, cax=cax)
+
+# parameters :
+m  = 100     # number of beta discretizations.
+n  = 100     # number of F discretizations.
+p  = 3       # number of parameters
+t0 = 0.0     # initial time
+tf = 40000   # final time
+dt = 500     # time step
+
+# Initial conditions
+y0 = [0.3, 0.3, 0.001]  
+
+# range of beta, flow, and time to model :
+betaMin = 0.0
+betaMax = 0.5
+Fmin    = 0.0
+Fmax    = 0.5
+
+beta_a  = linspace(betaMin, betaMax, m)
+F_a     = linspace(Fmin,    Fmax,    n)
+ta      = arange(t0, tf+dt, dt)
+
+# multiprocessing data structures :
+solvers = []
+queue   = []
+numCpus = cpu_count()
+Fs      = array_split(F_a,    numCpus)
+
+# create a solver for each processor and begin solving each :
+for i in range(numCpus):
+  q = Queue()
+  queue.append(q)
+  solver = solveProcess(i, q, beta_a, Fs[i], y0, ta, dt, p)
+  solvers.append(solver)
+  solver.start()
+
+# wait until solver (started above) finishes :
+for s in solvers:
+  s.join()
+
+# retrive the results :
+sols = []
+for q in queue:
+  while q.empty() == False:
+    sols.append(q.get())
+
+# put the results from the individual cores back together :
+for i, s in enumerate(sols):
+  if i == 0:
+    L_sol = s[0]
+    S_sol = s[1]
+    C_sol = s[2]
+  else:
+    L_sol = hstack((L_sol, s[0]))
+    S_sol = hstack((S_sol, s[1]))
+    C_sol = hstack((C_sol, s[2]))
+
+data = {'Beta'  : Beta,
+        'Flow'  : Flow,
+        'L_sol' : L_sol,
+        'S_sol' : S_sol,
+        'C_sol' : C_sol}
+savemat('../../killer_yeast/data/results.mat', data)
+
+# plot the results :
+fig = plt.figure()
+ax  = fig.add_subplot(111, projection='3d')
+
+Beta, Flow = meshgrid(beta_a, F_a)
+
+ax.plot_wireframe(Beta, Flow, L_sol, color='r', lw=2.0, rstride=5, cstride=5)
+ax.plot_wireframe(Beta, Flow, S_sol, color='k', lw=2.0, rstride=5, cstride=5)
+ax.set_xlabel(r'$\beta$')
+ax.set_ylabel(r'$F$')
 show()
+
+
+fig = plt.figure(figsize=(15,5))
+ax1 = fig.add_subplot(131)
+ax2 = fig.add_subplot(132)
+ax3 = fig.add_subplot(133)
+
+extent = [betaMin, betaMax, Fmin, Fmax]
+plot_sol(ax1, L_sol, extent, r'Killer',    cmap='Greys')
+plot_sol(ax2, S_sol, extent, r'Sensitive', cmap='Greys')
+plot_sol(ax3, C_sol, extent, r'Nutrient',  cmap='Greys')
+tight_layout()
+savefig('sols.png', dpi=300)
+show()
+
 
 
